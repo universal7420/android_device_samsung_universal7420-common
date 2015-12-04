22d7f9a68087d9c09a50e26117bfa1e35578d660
Say <sktjdgns1189@naver.com>
2015-12-05 00:21:11 +0900

    GPS Fix

--- device.mk
+++ device.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml
+    $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml \
+    $(LOCAL_PATH)/configs/lhd.conf:system/etc/lhd.conf
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
