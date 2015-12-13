41407c4b35fee79c439455797845df2376dffd08
Say <sktjdgns1189@naver.com>
2015-12-13 16:57:52 +0900

    init: switch to C++
    system/core/init switched to C++
    
    Signed-off-by: Say <sktjdgns1189@naver.com>

--- init/Android.mk
+++ init/Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_sec.c
+LOCAL_SRC_FILES := init_sec.cpp
 LOCAL_MODULE := libinit_sec
 include $(BUILD_STATIC_LIBRARY)
 
