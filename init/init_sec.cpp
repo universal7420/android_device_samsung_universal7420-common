11c6d0d2fd12c49927c4ad3ee7d97b9de1d11a4f
Say <sktjdgns1189@naver.com>
2015-12-22 00:15:12 +0900

    init: add G920I
    G920I is another very popular zerofltexx variant
    
    Signed-off-by: Say <sktjdgns1189@naver.com>

--- init/init_sec.cpp
+++ init/init_sec.cpp
@@ -66,6 +66,12 @@ void vendor_load_properties()
         property_set("ro.build.description", "zerofltelgt-user 5.1.1 LMY47X G920LXXS3COK5 release-keys");
         property_set("ro.product.model", "SM-G920L");
         property_set("ro.product.device", "zerofltelgt");
+    } else if (strstr(bootloader, "G920I")) {
+        /* zerofltexx */
+        property_set("ro.build.fingerprint", "samsung/zerofltexx/zerofltexx:5.1.1/LMY47X/G920IXXS3COK5:user/release-keys");
+        property_set("ro.build.description", "zerofltexx-user 5.1.1 LMY47X G920IXXS3COK5 release-keys");
+        property_set("ro.product.model", "SM-G920I");
+        property_set("ro.product.device", "zerofltexx");
     } else {
         /* zerofltexx */
         property_set("ro.build.fingerprint", "samsung/zerofltexx/zerofltexx:5.1.1/LMY47X/G920FXXS3COK5:user/release-keys");
