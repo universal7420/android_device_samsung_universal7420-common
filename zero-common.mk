76de747d2eeb02b87b76af54d240fdf1c8707251
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-02-10 21:13:38 -0500

    zero: Update aapt definitions
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -24,9 +24,14 @@ $(call inherit-product-if-exists, vendor/samsung/zero-common/zero-common-vendor.
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/zero-common/overlay
 
-# Device uses high-density artwork where available
+# This device is 640dpi.  However the platform doesn't
+# currently contain all of the bitmaps at 560dpi density so
+# we do this little trick to fall back to the xxhdpi version
+# if the 560dpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := 640dpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
