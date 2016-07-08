7c9f291ee63d8308f49e9e298279c6a09f240d65
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-07-08 00:57:33 -0400

    zero: Set EGL cache sizes
    Change-Id: Ifee0a4db8df497523e386d25adb220c7b7b41633
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,16 @@ BOARD_USES_SKIA_FIMGAPI := true
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # Graphics
 USE_OPENGL_RENDERER := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
