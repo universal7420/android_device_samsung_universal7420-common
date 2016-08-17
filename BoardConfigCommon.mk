1e54eba65f9d69d5e0ab5b63ccb8387fc0711659
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-08-16 22:44:05 -0400

    zero: Set backlight path for CM charger
    Change-Id: Ia0e4d843f2125ac0e2ea424c10d63fc740df7327
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,7 @@ TARGET_BOOTLOADER_BOARD_NAME := universal7420
 TARGET_NO_BOOTLOADER := true
 
 # Charger
+BACKLIGHT_PATH := /sys/devices/13900000.dsim/backlight/panel/brightness
 BOARD_BATTERY_DEVICE_NAME := battery
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
