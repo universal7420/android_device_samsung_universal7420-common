ef2a9a3ef31d510aeb974ab8af7ad50207ab5c7b
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-08-16 22:44:28 -0400

    zero: Clean up aapt pref trick comment
    Change-Id: I1bf9b89621f3ec06c2409f32977ab79b9f75a12d
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -28,9 +28,9 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/zero-common/overlay-gsm
 endif
 
 # This device is 640dpi.  However the platform doesn't
-# currently contain all of the bitmaps at 560dpi density so
+# currently contain all of the bitmaps at 640dpi density so
 # we do this little trick to fall back to the xxhdpi version
-# if the 560dpi doesn't exist.
+# if the 640dpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 640dpi
 # A list of dpis to select prebuilt apk, in precedence order.
