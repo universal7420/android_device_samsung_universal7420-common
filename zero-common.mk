b833fa76ee050fc5aecfdb3b486770e4e0a29992
Christopher N. Hesse <raymanfx@gmail.com>
2017-03-12 19:24:00 +0100

    zero: Enable fingerprint components
    Change-Id: Ie0eca5ec07020abc6a49ab4091a435012f33f4b6
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -108,6 +108,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml \
     $(LOCAL_PATH)/configs/lhd.conf:system/etc/lhd.conf
+    
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd \
+    libbauthtzcommon_shim
 
 # Graphics
 PRODUCT_PACKAGES += \
