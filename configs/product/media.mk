0d11f0047f10c3a44814b4e693cdd2b23647f51d
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    media: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    Merged-In: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Signed-off-by: Chippa-a <vusal1372@gmail.com>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- configs/product/media.mk
+++ configs/product/media.mk
@@ -18,7 +18,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:system/vendor/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/vendor/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:system/vendor/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
