3c0a092f0e95994747a0eb990979a0ac9670fa76
Lukas0610 <mail@lukasberger.at>
2017-05-24 09:40:34 +0200

    zero: symlink keylayout to fix softkeys on Edge
     * Created symlink sec_touchscreen.kl to sec_touchkey.kl
     * Thanks to our tester @Iceen

--- configs/product/keylayout.mk
+++ configs/product/keylayout.mk
@@ -17,6 +17,7 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+    $(LOCAL_PATH)/configs/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
     $(LOCAL_PATH)/configs/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
     $(LOCAL_PATH)/configs/idc/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
     $(LOCAL_PATH)/configs/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
