34580522c593140ed0e53fb4aa7072ac19fb99df
Lukas0610 <mail@lukasberger.at>
2017-05-30 21:05:00 +0200

    zero: added init.rilchip.rc

--- configs/product/ramdisk.mk
+++ configs/product/ramdisk.mk
@@ -21,6 +21,7 @@ PRODUCT_PACKAGES += \
     init.battery.rc \
     init.gps.rc \
     init.power.rc \
+    init.rilchip.rc \
     init.samsungexynos7420.rc \
     init.samsungexynos7420.usb.rc \
     init.wifi.rc \
--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -51,6 +51,14 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.rilchip.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.rilchip.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.wifi.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
