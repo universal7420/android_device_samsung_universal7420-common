941bafdd60db2ab194a3c0c941ac26128fb20386
Lukas0610 <mail@lukasberger.at>
2017-08-26 21:37:30 +0200

    zero: updated additional properties
    Change-Id: I5a7364749e9fac2093e34f3544f2eb2c65e83d39

--- configs/product/properties.mk
+++ configs/product/properties.mk
@@ -15,12 +15,6 @@
 # limitations under the License.
 #
 
-###############################
-# - by TeamNexus and former maintainers
-# - copied from https://androidforums.com/threads/build-prop-tweaks-edited.582603/
-# - copied from https://killertricks.com/build-prop-tweaks-for-android-devices/
-
-###############################
 # Common
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.chipname=exynos7420 \
@@ -31,117 +25,30 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.force_voice_config=wide \
     wifi.interface=wlan0
 
-###############################
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=288m \
-    dalvik.vm.heapsize=768m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m
-
-###############################
 # GPU
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.composition.type=dyn \
 	ro.opengles.version=196610 \
 	video.accelerate.hw=1 \
 	debug.egl.profiler=1
-	
+
+# H/W Composer
 ifneq ($(NEXUS_VERSION),)
-
-###############################
-# NexusOS
-PRODUCT_PROPERTY_OVERRIDES += \
-	debug.hwc.max_hw_overlays=1 \
-	debug.sf.hw=0 \
-	persist.sys.ui.hw=0 \
-	debug.egl.hw=0
-
+  PRODUCT_PROPERTY_OVERRIDES += \
+      debug.hwc.max_hw_overlays=1 \
+      debug.sf.hw=1 \
+      persist.sys.ui.hw=1 \
+      debug.egl.hw=1 \
+      debug.hwc.force_gpu=0
 else
-
-###############################
-# ResurrectionRemix/AOKP
-PRODUCT_PROPERTY_OVERRIDES += \
-	debug.hwc.max_hw_overlays=0 \
-	debug.sf.hw=1 \
-	persist.sys.ui.hw=1 \
-	debug.egl.hw=1
-
+  PRODUCT_PROPERTY_OVERRIDES += \
+      debug.hwc.max_hw_overlays=0 \
+      debug.sf.hw=0 \
+      persist.sys.ui.hw=0 \
+      debug.egl.hw=0 \
+      debug.hwc.force_gpu=1
 endif
 
-###############################
-# hwui
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hwui.use_gpu_pixel_buffers=true \
-	ro.hwui.texture_cache_size=176 \
-	ro.hwui.layer_cache_size=106 \
-	ro.hwui.path_cache_size=64 \
-	ro.hwui.shape_cache_size=16 \
-	ro.hwui.gradient_cache_size=8 \
-	ro.hwui.drop_shadow_cache_size=24 \
-	ro.hwui.r_buffer_cache_size=24 \
-	ro.hwui.text_small_cache_width=4096 \
-	ro.hwui.text_small_cache_height=4096 \
-	ro.hwui.text_large_cache_width=8192 \
-	ro.hwui.text_large_cache_height=8192
-
-###############################
-# Networking
-PRODUCT_PROPERTY_OVERRIDES += \
-	net.tcp.buffersize.default=4096,87380,256960, 4096, 16384,256960 \
-	net.tcp.buffersize.wifi=4096,87380,256960,409 6,163 84,256960 \
-	net.tcp.buffersize.umts=4096,8 7380,256960,4096,163 84,256960 \
-	net.tcp.buffersize.gprs=4096,8 7380,256960,4096,163 84,256960 \
-	net.tcp.buffersize.edge=4096,8 7380,256960,4096,163 84,256960
-
-###############################
 # OTA
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.nexus.otaname=zero
-
-###############################
-# Performance += \
-PRODUCT_PROPERTY_OVERRIDES += \
-	debug.performance.tuning=1
-
-###############################
-# Power-Management
-PRODUCT_PROPERTY_OVERRIDES += \
-	pm.sleep_mode=1
-
-###############################
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.ril.disable.power.collapse=1 \
-
-###############################
-# Stagefright
-PRODUCT_PROPERTY_OVERRIDES += \
-	media.stagefright.enable-player=true \
-	media.stagefright.enable-meta=true \
-	media.stagefright.enable-scan=true \
-	media.stagefright.enable-http=true \
-	media.stagefright.enable-rtsp=true \
-	media.stagefright.enable-record=false
-
-###############################
-# Surface
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.scrollingcache=4 \
-	persist.sys.purgeable_assets=1 \
-	ro.compcache.default=18 \
-	ro.max.fling_velocity=12000 \
-	ro.min.fling_velocity=8000
-
-###############################
-# Telephony
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.telephony.call_ring.delay=0 \
-	ring.delay=0
-
-###############################
-# Window-Manager
-PRODUCT_PROPERTY_OVERRIDES += \
-	windowsmgr.max_events_per_sec=90
