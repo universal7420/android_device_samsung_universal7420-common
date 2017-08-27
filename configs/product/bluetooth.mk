7ca71a6ff1590c51bd37b9fa14df0ed418d1d91c
Lukas0610 <mail@lukasberger.at>
2017-08-27 14:32:27 +0200

    zero: added missing libbt-vendor
    Change-Id: Icefaef2546e6d9cab11ef89cf4a25c2b2f0aaa7a

--- configs/product/bluetooth.mk
+++ configs/product/bluetooth.mk
@@ -16,7 +16,8 @@
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # [TEMP] Disable Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
