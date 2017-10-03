95d819758635b04f7073816bfad65072f4c6bc2d
Lukas0610 <mail@lukasberger.at>
2017-10-03 15:11:12 +0200

    zero: disable some input-devices on ambient display
    Change-Id: I5ffd960bce5d40d8504a34a15f080898db8f9c37

--- power/power.h
+++ power/power.h
@@ -73,7 +73,7 @@ static void power_boostpulse(int duration);
 /***********************************
  * Inputs
  */
-static void power_input_device_state(int state);
+static void power_input_device_state(int state, bool is_ambient_display);
 static void power_set_interactive(struct power_module __unused * module, int on);
 
 /***********************************
