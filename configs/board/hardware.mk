0ca70937f4afac12015c3a63ad30f9788a247433
Lukas0610 <mail@lukasberger.at>
2017-10-19 12:02:28 +0200

    zero: apply rebranding of unified cmhw to lineagehw

--- configs/board/hardware.mk
+++ configs/board/hardware.mk
@@ -15,7 +15,7 @@
 #
 
 # Hardware
-BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/lineagehw
 BOARD_HARDWARE_CLASS += device/samsung/zero-common/lineagehw
 
 # HWCServices
