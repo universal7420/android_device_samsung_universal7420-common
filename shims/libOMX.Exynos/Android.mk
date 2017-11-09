69a3dd990e5c271055e9cd855ba1b3831f070b6f
Lukas0610 <mail@lukasberger.at>
2017-11-09 22:07:55 +0100

    [TEMP] zero: disable experimental OMX re-bringup again
    This reverts commit a896f148bcd188ead350f8e4d0a9327126dbffbf.

--- shims/libOMX.Exynos/Android.mk
+++ shims/libOMX.Exynos/Android.mk
@@ -18,93 +18,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	libOMX.Exynos.cpp
+    libOMX.Exynos.cpp
 
-LOCAL_SHARED_LIBRARIES := \
-	libcutils \
-	libion_exynos \
-	liblog \
-	libui
-
-LOCAL_C_INCLUDES := \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-	frameworks/native/include/media/openmax/ \
-	hardware/samsung_slsi-cm/exynos/include \
-	hardware/samsung_slsi-cm/exynos/libvideocodec/include \
-	hardware/samsung_slsi-cm/$(TARGET_BOARD_PLATFORM)/include \
-	hardware/samsung_slsi-cm/$(TARGET_SOC)/include \
-	hardware/samsung_slsi-cm/openmax/component/common/ \
-	hardware/samsung_slsi-cm/openmax/include/ \
-	hardware/samsung_slsi-cm/openmax/include/exynos/ \
-	hardware/samsung_slsi-cm/openmax/osal/
-
-LOCAL_ADDITIONAL_DEPENDENCIES += \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-ifeq ($(BOARD_USE_ANB), true)
-    LOCAL_CFLAGS += -DUSE_ANB
-
-    ifneq ($(TARGET_SOC), exynos5410)
-        LOCAL_CFLAGS += -DUSE_ANB_REF
-    endif
-
-    ifeq ($(BOARD_USE_ANB_OUTBUF_SHARE), true)
-        LOCAL_CFLAGS += -DUSE_ANB_OUTBUF_SHARE
-    endif
-endif
-
-ifeq ($(BOARD_USE_DMA_BUF), true)
-    LOCAL_CFLAGS += -DUSE_DMA_BUF
-endif
-
-ifeq ($(BOARD_USE_METADATABUFFERTYPE), true)
-    LOCAL_CFLAGS += -DUSE_METADATABUFFERTYPE
-
-    ifeq ($(BOARD_USE_STOREMETADATA), true)
-        LOCAL_CFLAGS += -DUSE_STOREMETADATA
-    endif
-
-    ifeq ($(BOARD_USE_ANDROIDOPAQUE), true)
-        LOCAL_CFLAGS += -DUSE_ANDROIDOPAQUE
-    endif
-endif
-
-ifeq ($(BOARD_USE_IMPROVED_BUFFER), true)
-    LOCAL_CFLAGS += -DUSE_IMPROVED_BUFFER
-endif
-
-ifeq ($(BOARD_USE_CSC_HW), true)
-    LOCAL_CFLAGS += -DUSE_CSC_HW
-endif
-
-ifeq ($(BOARD_USE_NON_CACHED_GRAPHICBUFFER), true)
-    LOCAL_CFLAGS += -DUSE_NON_CACHED_GRAPHICBUFFER
-endif
-
-ifeq ($(BOARD_USE_NON_SECURE_DRM), true)
-    LOCAL_CFLAGS += -DUSE_NON_SECURE_DRM
-endif
-
-ifeq ($(TARGET_BOARD_PLATFORM),exynos3)
-    LOCAL_CFLAGS += -DUSE_MFC5X_ALIGNMENT
-endif
-
-ifeq ($(TARGET_BOARD_PLATFORM),exynos4)
-    LOCAL_CFLAGS += -DUSE_MFC5X_ALIGNMENT
-endif
-
-ifeq ($(BOARD_USE_ANDROID), true)
-LOCAL_C_INCLUDES += $(ANDROID_MEDIA_INC)/hardware
-endif
-
-ifeq ($(BOARD_USE_KHRONOS_OMX_HEADER), true)
-    LOCAL_CFLAGS += -DUSE_KHRONOS_OMX_HEADER
-    LOCAL_C_INCLUDES += $(EXYNOS_OMX_INC)/khronos
-else
-    ifeq ($(BOARD_USE_ANDROID), true)
-        LOCAL_C_INCLUDES += $(ANDROID_MEDIA_INC)/openmax
-    endif
-endif
+LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE := libOMX.Exynos_shim
 LOCAL_MODULE_TAGS := optional
