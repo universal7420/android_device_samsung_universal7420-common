3bdcd92c6138341e094ef7f23d37e63531462127
Lukas0610 <mail@lukasberger.at>
2017-12-07 15:31:48 +0100

    zero: added missing includes to camera/

--- camera/Android.mk
+++ camera/Android.mk
@@ -15,7 +15,9 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES += \
     system/core/include \
     system/media/camera/include \
-	frameworks/native/libs/arect/include
+	frameworks/native/libs/arect/include \
+	frameworks/native/libs/nativebase/include \
+	frameworks/native/libs/nativewindow/include
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 
