192d5c15a8e83da356b45c4c688b8556d859f7af
Lukas0610 <mail@lukasberger.at>
2017-12-12 10:00:11 +0100

    zero: updated GPS configuration and modules
    Change-Id: I9c4f9cc07431e81c5d4e8fa2df286ce4361078d7

--- configs/product/gps.mk
+++ configs/product/gps.mk
@@ -14,6 +14,12 @@
 # limitations under the License.
 #
 
+# HIDL
+PRODUCT_PACKAGES += \
+	android.hardware.gnss@1.0-impl \
+	android.hardware.gnss@1.0-service
+
+# Shims
 PRODUCT_PACKAGES += \
     gpsd_shim
 
