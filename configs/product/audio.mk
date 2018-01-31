0dbce53897558a9af6bdfaf9d0988e73052c2e3a
Lukas0610 <mail@lukasberger.at>
2018-01-31 21:09:55 +0100

    zero: do not use binderized audio-service
    may interfer with libsec-ril(-dsds) IPC access-control
    
    Change-Id: I20d61a212e1640832693c54133b3765f228edb14

--- configs/product/audio.mk
+++ configs/product/audio.mk
@@ -29,5 +29,4 @@ PRODUCT_PACKAGES += \
     tinymix \
     libtinycompress \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl
