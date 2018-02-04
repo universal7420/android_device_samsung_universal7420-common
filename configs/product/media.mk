8314dc5585637bcbdd2d204a1e1fb0de26523e90
Lukas0610 <mail@lukasberger.at>
2018-02-04 09:51:35 +0100

    Revert "zero: moved properties to vendor, removed unused ones"
    This reverts commit d68d0391acfcc42c82a7100c723112ee09b857b4.

--- configs/product/media.mk
+++ configs/product/media.mk
@@ -22,3 +22,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
+
+# media build properties
+PRODUCT_PROPERTY_OVERRIDES += \
+	media.sf.omx-plugin=libffmpeg_omx.so,libsomxcore.so \
+    persist.media.treble_omx=true
