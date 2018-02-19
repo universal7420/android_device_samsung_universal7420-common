6da7f65cb516dab55e2a8f7d99bda3aefb2ca6e4
Lukas0610 <mail@lukasberger.at>
2018-02-19 18:04:54 +0100

    zero: only fail storeMetaDataInBuffers-check for decoders
    Change-Id: I6ecd22e0f0238dbd51dea1f92b98daad09d7fa99

--- shims/libExynosOMX/Exynos_OMX_VdecControl.c
+++ shims/libExynosOMX/Exynos_OMX_VdecControl.c
@@ -17,15 +17,45 @@
 
 #define LOG_TAG "libExynosOMX_shim"
 
+#include <dlfcn.h>
 #include <utils/Log.h>
 
 int Exynos_OSAL_Strcmp(const char *s1, const char *s2)
 {
+	void *ptr;
+	int ret;
+	Dl_info info;
+
+	/* get address of parent function */
+	ptr = __builtin_return_address(0);
+
+	/* skip index-check if we couldn't get return-address */
+	if (!ptr) {
+		ALOGE("%s: failed to retrieve return address", __func__);
+		goto exit;
+	}
+
+	/* get infos about parent function */
+	ret = dladdr(ptr, &info);
+
+	/* skip index-check if we couldn't get infos about parent function */
+	if (!ret) {
+		ALOGE("%s: failed to retrieve informations about parent function", __func__);
+		goto exit;
+	}
+	
+	/* check if the parent function is Exynos_OMX_VideoDecodeGetExtensionIndex() */
+	if (strcmp(info.dli_sname, "Exynos_OMX_VideoDecodeGetExtensionIndex")) {
+		/* no log here... */
+		goto exit;
+	}
+
 	/* prevent check for storeMetaDataInBuffers-support to succeed */
 	if (!strcmp(s1, "OMX.google.android.index.storeMetaDataInBuffers")) {
 		ALOGI("%s: failing check for storeMetaDataInBuffers-support", __func__);
 		return -1;
 	}
 
+exit:
 	return strcmp(s1, s2);
 }
