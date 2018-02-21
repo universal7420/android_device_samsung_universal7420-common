15615038914b74b6666878670d88f32d9931dc8b
Lukas0610 <mail@lukasberger.at>
2018-02-21 11:21:08 +0100

    zero: add releasetools-extension for resizing /system/
    Change-Id: I1f1b16df65dc67a09277c50f16759d87c8671e21

--- configs/product/recovery.mk
+++ configs/product/recovery.mk
@@ -14,14 +14,7 @@
 # limitations under the License.
 #
 
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7420.recovery
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_HAS_DOWNLOAD_MODE := true
-
-# Releasetools - for resizing system filesystem
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/zero-common/releasetools
+# Filesystem tools for resizing system partition filesystem
+PRODUCT_PACKAGES += \
+	e2fsck_static \
+	resize2fs_static
