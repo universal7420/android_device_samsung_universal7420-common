ecfc2a15b01be43b85a3c7ff0bb54feecaccd5c5
Lukas0610 <mail@lukasberger.at>
2018-03-08 14:47:44 +0100

    zero: fetch private vendor aspects if available
    Change-Id: I3950860e143ffc8daaffec35aeabb2297bde7bb1

--- zero-common.mk
+++ zero-common.mk
@@ -25,5 +25,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
 $(call inherit-product, hardware/samsung_slsi-cm/exynos7420/exynos7420.mk)
 
+# Get private non-open-source blobs
+$(call inherit-product-if-exists, vendor/samsung/zero-private/zero-private-vendor.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/samsung/zero-common/zero-common-vendor.mk)
