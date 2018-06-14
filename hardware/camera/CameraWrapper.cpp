8f66dfd60243a386a0e83fc52aac227437b36cf1
Lukas0610 <mail@lukasberger.at>
2018-06-14 18:45:05 +0200

    zero: camera: align with default HAL and add some debugging
    Change-Id: Ibfea6e380b44a287378fe983577ea38ec5446458

--- hardware/camera/CameraWrapper.cpp
+++ hardware/camera/CameraWrapper.cpp
@@ -62,7 +62,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
     .get_vendor_tag_ops = camera_get_vendor_tag_ops,
     .open_legacy = camera_open_legacy,
     .set_torch_mode = camera_set_torch_mode,
-    .init = NULL,
+    .init = camera_init,
     .reserved = {0},
 };
 
@@ -126,4 +126,12 @@ static int camera_set_torch_mode(const char* camera_id, bool enabled)
     if (check_vendor_module())
         return 0;
     return gVendorModule->set_torch_mode(camera_id, enabled);
+}
+
+static int camera_init(void)
+{
+    ALOGV("%s", __FUNCTION__);
+    if (check_vendor_module())
+        return 0;
+    return gVendorModule->init();
 }
