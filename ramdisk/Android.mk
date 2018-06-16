74ad7e7ad949d4983515f0f59659dcd3975045d7
Lukas0610 <mail@lukasberger.at>
2018-06-16 23:38:43 +0200

    zero: add missing build-module for init.samsungexynos7420.rc
    Change-Id: I597b97086df2c0ff3da35772e4f5a8b4396030cc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -67,6 +67,14 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.samsungexynos7420.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= init.samsungexynos7420.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.samsungexynos7420.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
