165f28c00192ef2d4ba4709b73f6359baf08d1b3
Lukas0610 <mail@lukasberger.at>
2018-06-18 21:21:17 +0200

    zero: auto-generate nolog property list
    Change-Id: I0f054a50ef61cc91b23318c158e1ddae6060d052

---
+++ configs/logging/Android.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2018 TeamNexus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+INSTALLED_ZERO_NOLOG_PROP_TARGET := $(TARGET_OUT_VENDOR)/nolog.prop
+INSTALLED_ZERO_NOLOG_PROP_SOURCE := device/samsung/zero-common/configs/logging/nolog.txt
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_ZERO_NOLOG_PROP_TARGET)
+
+# First, read the list
+$(INSTALLED_ZERO_NOLOG_PROP_TARGET): $(INSTALLED_ZERO_NOLOG_PROP_SOURCE)
+	@echo Generating zero-nolog properties: $@
+	$(hide) echo "#" > $@; \
+	        echo "# ZERO NOLOG PROPERTIES" >> $@; \
+	        echo "# auto-generated - do not edit" >> $@; \
+	        echo "#" >> $@;
+	$(hide) cat $(INSTALLED_ZERO_NOLOG_PROP_SOURCE) | \
+	        grep -E '^[^#]' | \
+	        sort | \
+	        awk '{ printf "log.tag.%s=S\n", $$1 }' >> $@
