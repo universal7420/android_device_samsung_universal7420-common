3e22aa10220655af182bc64487018053cbb67d79
Lukas0610 <mail@lukasberger.at>
2018-08-07 08:51:06 +0200

    zero: doze: define SDK version
    Change-Id: Ib3cc94a1c5ffb8a29b82a1321accdca86c919e13

--- doze/Android.mk
+++ doze/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := ZeroDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := current
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
