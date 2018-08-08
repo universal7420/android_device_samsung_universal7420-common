e5ef3113ec3ee2cb2d3dc90623ad2f5ed82f95d1
Lukas0610 <mail@lukasberger.at>
2018-08-08 10:55:19 +0200

    zero: import customized android.hardware.camera.provider@2.4 implementation
    Removed references to
    
    	camera.device@3.3-impl
    	camera.device@3.4-impl
    	camera.device@3.4-external-impl
    
    Change-Id: Iddacbec90e31761520a03001134d4e0ca58cf7ac

--- zero-common.mk
+++ zero-common.mk
@@ -74,9 +74,7 @@ TARGET_SCREEN_WIDTH := 1440
 ## Packages
 PRODUCT_PACKAGES += \
 	libexynoscamera_shim \
-	camera.device@1.0-impl \
-	camera.device@3.2-impl \
-	android.hardware.camera.provider@2.4-impl \
+	android.hardware.camera.provider@2.4-impl.zero \
 	android.hardware.camera.provider@2.4-service
 
 ## Apps
