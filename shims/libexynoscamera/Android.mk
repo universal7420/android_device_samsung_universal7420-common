aa5eb7b6819fe27e2037fc6da2a5a0a279459cd3
Lukas0610 <mail@lukasberger.at>
2018-08-12 01:04:46 +0200

    zero: update libexynoscamera shim to Android P
      - Split up shim source files
    
      - Included android::Fence::~Fence()-symbol
    
    Change-Id: I0668589c7e305bc492a488de72b0cd7470b4b498

--- shims/libexynoscamera/Android.mk
+++ shims/libexynoscamera/Android.mk
@@ -16,7 +16,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    libexynoscamera.cpp
+    CameraParameters.cpp \
+    Fence.cpp
 
 LOCAL_SHARED_LIBRARIES := liblog
 
---
+++ shims/libexynoscamera/Fence.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2018 TeamNexus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+extern "C"
+{
+
+//
+// External deconstructor symbol got removed in P
+// Replaced with empty default implementation:
+//
+//     ~Fence() = default;
+//
+void *_ZN7android5FenceD1Ev(void *_this) {
+	return _this;
+}
+
+}
