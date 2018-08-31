1fde57cdef8dd5e66571aafff89d1200c73a5774
Lukas0610 <mail@lukasberger.at>
2018-08-31 14:37:09 +0200

    zero: sync wifi-config with hero
    Change-Id: I640317d75ffb56f72fa888373c40fd0434fe6b68

--- zero-common.mk
+++ zero-common.mk
@@ -428,7 +428,6 @@ PRODUCT_PACKAGES += \
 #
 ## Configs
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/vendor/etc/wifi/wpa_supplicant.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf \
 	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
 	$(LOCAL_PATH)/configs/wifi/filter_ie:system/etc/wifi/filter_ie
