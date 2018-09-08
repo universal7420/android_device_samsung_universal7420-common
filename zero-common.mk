3b7563097dd90abcc60d477e5ed58c149e4cfe1d
Lukas0610 <mail@lukasberger.at>
2018-09-08 15:15:15 +0200

    zero: switch to basic USB HAL
    Change-Id: Ia213dfb383ea2ea987a4d30350f974ad31e18e51

--- zero-common.mk
+++ zero-common.mk
@@ -414,8 +414,7 @@ PRODUCT_PACKAGES += \
 # USB
 #
 PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-impl \
-	android.hardware.usb@1.0-service
+	android.hardware.usb@1.0-service.basic
 
 #
 # Vibrator
