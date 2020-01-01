420e2c66e7118b291bf8dc4297d45635805b0640
Lunarixus <Nathanb644@hotmail.co.uk>
2020-01-01 22:34:38 +0000

    zero-common: Update WiFi configuration
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -378,6 +378,7 @@ BOARD_USES_WFD := true
 #
 BOARD_HAVE_SAMSUNG_WIFI          := true
 BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_USE_HIDL          := true
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 WPA_SUPPLICANT_USE_HIDL          := true
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
@@ -385,8 +386,8 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
 
 # inherit NexusOS-branding, apps, etc.
