78405d75ff78ee9018193a5f0d294b3b1912b1a1
Lunarixus <Lunarixus@raphielgang.org>
2020-02-16 17:28:43 +0000

    zero-common: ramdisk: Drop eng module tags
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.samsungexynos7420
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= fstab.samsungexynos7420
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -10,7 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.baseband.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
   ifneq ($(filter zerofltespr zeroltespr,$(TARGET_DEVICE)),)
     LOCAL_SRC_FILES       := init.baseband.rc.cdma
@@ -22,7 +22,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.rilchip.prop
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
   ifneq ($(filter zerofltespr zeroltespr,$(TARGET_DEVICE)),)
     LOCAL_SRC_FILES       := init.rilchip.prop.cdma
@@ -34,7 +34,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.battery.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.battery.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
@@ -42,7 +42,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.power.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.power.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
@@ -50,7 +50,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.rilchip.rc
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.rilchip.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
@@ -58,7 +58,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.rilchip.sh
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.rilchip.sh
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
@@ -66,7 +66,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.samsungexynos7420.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= init.samsungexynos7420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -74,7 +74,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.samsungexynos7420.usb.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= init.samsungexynos7420.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -82,7 +82,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.recovery.samsungexynos7420.rc
-LOCAL_MODULE_TAGS   := eng
 LOCAL_MODULE_CLASS  := RECOVERY_EXECUTABLES
 LOCAL_SRC_FILES		:= init.recovery.samsungexynos7420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -90,7 +89,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
