c9f5aa4454d41755c6cff023f22485671861538f
Lunarixus <Lunarixus@raphielgang.org>
2020-02-17 19:21:00 +0000

    zero-common: Build vibrator packages
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- zero-common.mk
+++ zero-common.mk
@@ -437,6 +437,14 @@ PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service.basic
 
 #
+# Vibrator
+#
+PRODUCT_PACKAGES += \
+	android.hardware.vibrator@1.0-impl \
+	android.hardware.vibrator@1.0-service
+
+
+#
 # Wifi
 #
 ## Configs
