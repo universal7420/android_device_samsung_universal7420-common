f160269ef39ba5ccca009b0b6fac7b9be3782c48
Lunarixus <Lunarixus@raphielgang.org>
2020-02-17 16:56:55 +0000

    zero-common: Return to official GNSS impl
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- zero-common.mk
+++ zero-common.mk
@@ -162,7 +162,7 @@ PRODUCT_PACKAGES += \
 #
 ## HIDL
 PRODUCT_PACKAGES += \
-	android.hardware.gnss@1.0-impl.zero \
+	android.hardware.gnss@1.0-impl \
 	android.hardware.gnss@1.0-service
 
 ## Configs
