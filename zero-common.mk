7a8eda38c4b5b96884fa5225d0f9ac66cd9f7ffc
Lunarixus <Lunarixus@raphielgang.org>
2020-02-18 04:09:22 +0000

    zero-common: Use GPS init from S7
    - Thanks to Ivan for this.
    
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- zero-common.mk
+++ zero-common.mk
@@ -166,6 +166,7 @@ PRODUCT_PACKAGES += \
 ## Configs
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+	$(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml \
 	$(LOCAL_PATH)/configs/gps/gnss.conf:system/etc/gnss.conf
 
 #
