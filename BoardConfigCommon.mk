a9f8e746f60582e165f9e178c12bdeff20a0154e
Lunarixus <Lunarixus@raphielgang.org>
2020-02-18 16:31:24 +0000

    zero-common: Shim correct fingerprint blob
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,7 +149,7 @@ TARGET_SEC_FP_HAL_VARIANT := bauth
 
 ## Shims
 TARGET_LD_SHIM_LIBS += \
-	/system/lib64/hw/fingerprint.vendor.exynos5.so|/vendor/lib64/libbauthtzcommon_shim.so
+	/system/lib64/libbauthserver.so|/vendor/lib64/libbauthtzcommon_shim.so|/vendor/lib64/libbauthtzcommon_shim.so
 
 #
 # GPS
