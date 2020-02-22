fe769c4a367b482d6583524337d3e2444f3d81dc
Lunarixus <Lunarixus@raphielgang.org>
2020-02-22 17:36:23 +0000

    zero-common-override: Reset back to old override makefile
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- zero-common-override.mk
+++ zero-common-override.mk
@@ -15,10 +15,32 @@
 # limitations under the License.
 #
 
-# UI performance tweaks
-PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.latch_unsignaled=1
-	debug.sf.disable_backpressure=1
-	hwui.disable_vsync=true
-	persist.sys.ui.hw=1
-	debug.egl.hw=1
+zero_build_fingerprint := "Samsung/$(PRODUCT_NAME)/$(PRODUCT_NAME):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/test-keys"
+zero_build_flavor      := "$(PRODUCT_NAME)-$(TARGET_BUILD_VARIANT)"
+zero_build_descr       := "$(zero_build_flavor) $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) test-keys"
+
+__PRODUCT_TARGET_NAME := $(PRODUCT_NAME)
+
+# Override build-properties to avoid too long fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_DISPLAY_ID=$(zero_build_descr) \
+    PRIVATE_BUILD_DESC=$(zero_build_descr) \
+    BUILD_FINGERPRINT_FROM_FILE=$(zero_build_fingerprint) \
+	TARGET_BUILD_FLAVOR="$(zero_build_flavor)"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := $(zero_build_fingerprint)
+
+# Set OTA-required properties in default.prop
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.product.name=$(PRODUCT_NAME) \
+	ro.product.device=$(PRODUCT_DEVICE) \
+	ro.product.brand=$(PRODUCT_BRAND) \
+	ro.product.manufacturer=$(PRODUCT_MANUFACTURER) \
+	ro.product.model=$(PRODUCT_MODEL) \
+	ro.vendor.product.name=$(PRODUCT_NAME) \
+	ro.vendor.product.device=$(PRODUCT_DEVICE) \
+	ro.vendor.product.brand=$(PRODUCT_BRAND) \
+	ro.vendor.product.manufacturer=$(PRODUCT_MANUFACTURER) \
+	ro.vendor.product.model=$(PRODUCT_MODEL)
+
