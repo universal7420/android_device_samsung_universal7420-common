20944317ae6b82861ce86396a066be9173f16646
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2020-03-06 17:48:22 -0800

    zero-common: Drop universal7420 lights for Samsung lights

--- zero-common.mk
+++ zero-common.mk
@@ -235,9 +235,7 @@ PRODUCT_PACKAGES += \
 # Lights
 #
 PRODUCT_PACKAGES += \
-	android.hardware.light@2.0-impl \
-	android.hardware.light@2.0-service \
-	lights.universal7420
+	android.hardware.light@2.0-service.samsung
 
 #
 # Media
