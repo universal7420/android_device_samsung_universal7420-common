29ea8e74eab9bf568e1025410c480639cddf62f6
Lunarixus <Nathanb644@hotmail.co.uk>
2020-04-16 22:58:41 +0000

    zero-common: Update graphics packages
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- zero-common.mk
+++ zero-common.mk
@@ -173,15 +173,15 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml \
 	$(LOCAL_PATH)/configs/gps/gnss.conf:system/etc/gnss.conf
 
-#
 # Graphics
-#
 PRODUCT_PACKAGES += \
-	libhwc2on1adapter \
-	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.mapper@2.0-impl
+    libion \
+    libfimg \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    libhwc2on1adapter
 
 #
 # Init
