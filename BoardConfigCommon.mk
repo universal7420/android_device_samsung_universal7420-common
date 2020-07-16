6c4225c0a0b16765f1230a91fc002018265ae2ee
ripee <ripee.dev@gmail.com>
2020-07-16 15:38:19 +0200

    universal7420-common: Set BOARD_HAVE_SAMSUNG_BLUETOOTH to true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,6 +69,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/include/hardware
 BOARD_CUSTOM_BT_CONFIG := $(LOCAL_PATH)/configs/bluetooth/libbt_vndcfg.txt
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_SAMSUNG_BLUETOOTH := true
 
 #
 # Bootanimation
